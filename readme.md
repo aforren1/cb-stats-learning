7acafbc047fb1c96ddc9794233f9ff01ac9576bc
Alex Forrence <alex.forrence@gmail.com>
2021-04-09 13:27:58 -0400

    partway through trial generation-- exposure constraints done?

--- readme.md
+++ readme.md
@@ -2,13 +2,28 @@ https://docs.google.com/drawings/d/1e_DbdycUV3cR6wGl0nLgx05iv2e5GRe7jKPuSzC3bpw/
 
 https://docs.google.com/document/d/18J9oLLlHHtOEKnxLaZg8jFG2Q7HZpozbR0I2jkwAf5U/edit
 
-use ←, ↓, → arrows (down arrow for cover task, L/R for probe)
+use ←, space, → keys (space for cover task, L/R for probe)
 
 Spritesheet has 100x100 images, 24 images total (see e.g. https://labs.phaser.io/edit.html?src=src/loader/sprite%20sheet/load%20sprite%20sheet.js&v=3.54.0 for spritesheet loading)
 
 (TODO: scale images up a little more, and add spacing between so that antialias doesn't lead to bleeding of nearby pixels in the sheet)
 (or keep as pixel art, so the patch actually stays intact)
 Should target IE? Older people, older browsers
+
+Font at least 19px (for old folks), and see here for more guidance wrt line spacing, etc. https://health.gov/healthliteracyonline/display/section-3-3/
+
+# Order of events
+
+- Initial instructions
+- 10 trials with 4 noise, 1.5-2 sec exposure and 0.5sec ISI (0.25sec online feedback)
+- Feedback, now warn faster & no feedback
+- 10 trials with 4 noise, 0.5 sec exposure and 0.5sec ISI (no online feedback)
+- Feedback, now real deal (n minutes continuous, no breaks)
+- 288 trials, 58 noise (0.5 exposure, 0.5 ISI)
+- Feedback, click to continue
+- Instructions (now we'll see if certain sets of stimuli more familiar)
+- Triplet tests, with left/right selection & down button to continue
+- Done, thx
 
 # Learning/Exposure Phase
 
@@ -25,11 +40,11 @@ Should target IE? Older people, older browsers
 
 # Test phase
 
-- Counterbalance left vs right shown first, and foil vs real shown first (4 repeats, so can do one of each)
+- Counterbalance left vs right shown first, and foil vs real shown first (8 repeats, so can do 2 of each)
 - Be clear that left arrow means left side, right means right (one person from original did left for first, right for second)
 - Press L/R to choose, down arrow to continue to next trial
 - 0.5s exposure + 0.5s ISI for each triplet presentation, with 1s between presentation
 - Add fixation cue to presentation side 1s before presentation
 - down button hit -> 1s delay (show fix cue on side) -> triplet presentation -> 1s delay (cue on opposite side) -> triplet presentation
-- If not sure, guess!
+- If not sure, encourage guessing!
 -
---
+++ src/utils/trialgen.js
@@ -0,0 +1,157 @@
+/*
+For instructions:
+{
+    trial_type
+    instruct_type (initial_practice, faster_practice, exposure, test (end will be baked in))
+    ~instruct_text~ (would want to embed, but we want to give user-specific feedback for first practice & no great sprintf equivalent in JS)
+    // user data (necessary? probably nice-to-have in case excessive time between exposure & test...)
+    start_time (relative to beginning of section)
+}
+For practice/exposure:
+{
+    trial_type (instruct, practice, exposure, test)
+    stimulus_index (stimulus shown, 0-23)
+    stimulus_id (A, B, C, ...)
+    cover_pos (x, y or null) (if null, no cover shown)
+    exposure_time (millis)
+    feedback_time (millis or null) (if null, no feedback)
+    iti_time (millis)
+    triplet_id (e.g. ABC, DEG) ()
+    // user data
+    reaction_time(s) (list of millis or null if no resp)
+    correct (pressed & cover image present, no press & cover image absent)
+}
+
+For test:
+{
+    trial_type
+    left_triplet (e.g. ABC)
+    right_triplet (e.g. LMN)
+    first_side_shown (left/right)
+    familiar_side (left/right) (foil on opposite side)
+    // user data
+    chosen_side (left/right)
+    reaction_time (from end of animating the second triplet)
+}
+*/
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+}
+
+const stim_ids = 'ABCDEFGHIJKLMNOPQRSTUVWX'
+export default function makeTrials(debug) {
+  // first, generate the triplets of interest
+  let stim_id_list = stim_ids.split('')
+  shuffleArray(stim_id_list)
+  // take 3 at a time
+  let familiar_triplets = []
+  for (let i = 0; i < 12; i += 3) {
+    familiar_triplets.push(stim_id_list.slice(i, i + 3))
+  }
+  // now generate foils (3 unique items from 3 different triplets,
+  // elements never appearing consecutively)
+  let foil_triplets = []
+  for (let i = 0; i < 4; i++) {
+    foil_triplets.push([
+      familiar_triplets[i % 4][0],
+      familiar_triplets[(i + 1) % 4][1],
+      familiar_triplets[(i + 2) % 4][2],
+    ])
+  }
+
+  // for practice stim, use each one twice
+  let practice_stim = stim_id_list.slice(12, 22)
+  //
+  let trial_list = []
+  let trial_counter = 0
+  // practice 1 (easy, online feedback)
+  trial_list.push({
+    trial_type: 'instruct',
+    instruct_type: 'initial_practice',
+  })
+  shuffleArray(practice_stim)
+  let tmp_inds = [1, 4, 7, 8]
+  for (let i of practice_stim) {
+    let pos = tmp_inds.includes(trial_counter) ? [Math.random() * 100 - 50, Math.random() * 100 - 50] : null
+    trial_list.push({
+      trial_type: 'practice',
+      stimulus_index: stim_ids.indexOf(i),
+      stimulus_id: i,
+      cover_pos: pos,
+      exposure_time: 1500,
+      feedback_time: 500,
+      iti_time: 1000,
+      triplet_id: null,
+      trial_number: trial_counter,
+    })
+    trial_counter++
+  }
+  // practice 2 (harder, no feedback)
+  trial_list.push({
+    trial_type: 'instruct',
+    instruct_type: 'faster_practice',
+  })
+  shuffleArray(practice_stim)
+  let tmp_inds = [12, 13, 15, 16]
+  for (let i of practice_stim) {
+    let pos = tmp_inds.includes(trial_counter) ? [Math.random() * 100 - 50, Math.random() * 100 - 50] : null
+    trial_list.push({
+      trial_type: 'practice',
+      stimulus_index: stim_ids.indexOf(i),
+      stimulus_id: i,
+      cover_pos: pos,
+      exposure_time: 500,
+      feedback_time: null,
+      iti_time: 500,
+      triplet_id: null,
+      trial_number: trial_counter,
+    })
+    trial_counter++
+  }
+  // exposure
+  trial_list.push({
+    trial_type: 'instruct',
+    instruct_type: 'exposure',
+  })
+  // generate order of triplets (AA and ABAB disallowed)
+  // divide section into thirds, so 8 triplet repeats per third
+  // respect repeats across thirds boundaries
+  let base = Array(8).fill([0, 1, 2, 3]).flat()
+  let triplet_sequence = []
+  loop1: for (let i = 0; i < 3; i++) {
+    loop2: while (true) {
+      shuffleArray(base)
+      // check constraints
+      loop3: for (let idx = 0; idx < base.length; idx++) {
+        // AA not allowed
+        if (idx > 0 && base[idx] === base[idx - 1]) {
+          continue loop2
+        }
+        // ABAB not allowed
+        if (idx > 2 && base[idx] === base[idx - 2] && base[idx - 1] === base[idx - 3]) {
+          continue loop2
+        }
+        // check thirds violations
+        if (triplet_sequence.length > 0) {
+          // AA not allowed
+          let len = triplet_sequence.length
+          if (base[idx] === triplet_sequence[len - 1]) {
+            continue loop2
+          }
+          if (idx > 0 && base[idx] === triplet_sequence[len - 1] && base[idx - 1] === triplet_sequence[len - 2]) {
+            continue loop2
+          }
+        }
+      }
+      break loop2
+    }
+    // make a copy so we can reuse base
+    triplet_sequence.push(base.slice())
+  }
+  // should be good to go now, flatten the sequence
+  triplet_sequence = triplet_sequence.flat()
+}
